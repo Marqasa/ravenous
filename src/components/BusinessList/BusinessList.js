720e486b149b3d9cb98f6cc42c1833c2db6721db
Marqasa <43411403+Marqasa@users.noreply.github.com>
2020-11-04 20:39:03 +0000

    Prepare components to send and receive props

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -3,18 +3,15 @@ import './BusinessList.css';
 import Business from '../Business/Business';
 
 class BusinessList extends React.Component {
-    render() {
-        return (
-            <div className="BusinessList">
-                <Business />
-                <Business />
-                <Business />
-                <Business />
-                <Business />
-                <Business />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="BusinessList">
+        {this.props.businesses.map(business => {
+          return <Business business={business} />;
+        })}
+      </div>
+    );
+  }
 }
 
 export default BusinessList;
