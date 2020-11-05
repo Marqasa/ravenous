82f5466218c7206d7746b034c1ff4dd19958c5f0
Marqasa <43411403+Marqasa@users.noreply.github.com>
2020-11-05 18:01:50 +0000

    Add 'state' functionality

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
-Ravenous is a web application built using the React.js framework. It functions simmilarly to Yelp and allows users to:
-  • Search for restaurants
-  • View a list of restaurants returned by the Yelp API
-  • Sort through restaurants using a filter
-  
+# Ravenous
+
+Ravenous is a web application built using the React.js framework.
+
+## Functionality
+
+Ravenous functions similarly to Yelp and allows users to:
+
+• Search for restaurants
+
+• View a list of restaurants returned by the Yelp API
+
+• Sort through restaurants using a filter
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,17 +1,71 @@
-import React from 'react';
-import './SearchBar.css';
-
-const sortByOptions = {
-  'Best Match': 'best_match',
-  'Highest Rated': 'rating',
-  'Most Reviewed': 'review_count'
-};
+import React from "react";
+import "./SearchBar.css";
 
 class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      term: "",
+      location: "",
+      sortBy: "best_match",
+    };
+
+    this.handleTermChange = this.handleTermChange.bind(this);
+    this.handleLocationChange = this.handleLocationChange.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
+
+    this.sortByOptions = {
+      "Best Match": "best_match",
+      "Highest Rated": "rating",
+      "Most Reviewed": "review_count",
+    };
+  }
+
+  getSortByClass(sortByOption) {
+    return this.state.sortBy === sortByOption ? "active" : "";
+  }
+
+  handleSortByChange(sortByOption) {
+    this.setState({
+      sortBy: sortByOption,
+    });
+  }
+
+  handleTermChange(event) {
+    this.setState({
+      term: event.target.value,
+    });
+  }
+
+  handleLocationChange(event) {
+    this.setState({
+      location: event.target.value,
+    });
+  }
+
+  handleSearch(event) {
+    this.props.searchYelp(
+      this.state.term,
+      this.state.location,
+      this.state.sortBy
+    );
+    event.preventDefault();
+  }
+
   renderSortByOptions() {
-    return Object.keys(sortByOptions).map(sortByOption => {
-      const sortByOptionValue = sortByOptions[sortByOption];
-      return <li key={sortByOptionValue}>{sortByOption}</li>;
+    return Object.keys(this.sortByOptions).map((sortByOption) => {
+      const sortByOptionValue = this.sortByOptions[sortByOption];
+
+      return (
+        <li
+          className={this.getSortByClass(sortByOptionValue)}
+          key={sortByOptionValue}
+          onClick={this.handleSortByChange.bind(this, sortByOptionValue)}
+        >
+          {sortByOption}
+        </li>
+      );
     });
   }
 
@@ -19,15 +73,16 @@ class SearchBar extends React.Component {
     return (
       <div className="SearchBar">
         <div className="SearchBar-sort-options">
-          <ul>
-            {this.renderSortByOptions()}
-          </ul>
+          <ul>{this.renderSortByOptions()}</ul>
         </div>
         <div className="SearchBar-fields">
-          <input placeholder="Search Businesses" />
-          <input placeholder="Where?" />
+          <input
+            onChange={this.handleTermChange}
+            placeholder="Search Businesses"
+          />
+          <input onChange={this.handleLocationChange} placeholder="Where?" />
         </div>
-        <div className="SearchBar-submit">
+        <div className="SearchBar-submit" onClick={this.handleSearch}>
           <a>Let's Go</a>
         </div>
       </div>
